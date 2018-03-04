913e1223bb58d075495fc1df1de1e652d6a65cbe
Dhans A <dhansking@gmail.com>
2018-03-03 20:32:36 -0600

    for chef learn rally

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package 'httpd' do
+action [:install]
+end
+
+group 'dhans'
+
+user 'dhans' do
+	group 'dhans'
+	system true
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '700'
+  user 'dhans'
+  group 'dhans'
+end
